4c501776982b5d178b5e061b703c04fbb7af6edc
Dmitry Gamanenko <dmitry.gamanenko@raccoongang.com>
2016-05-19 21:35:59 +0300

    upd

--- openedx-fullstack-ubuntu/install-openedx.sh
+++ openedx-fullstack-ubuntu/install-openedx.sh
@@ -12,19 +12,20 @@ EDXAPP_SITE_NAME=$3
 EDXAPP_CMS_SITE_NAME=$4
 EDXAPP_PREVIEW_LMS_BASE=$5
 
-bash -c "cat <<EOF >>server-vars.yml
-EDXAPP_PLATFORM_NAME: \"$EDXAPP_PLATFORM_NAME\"
-EDXAPP_SITE_NAME: \"$EDXAPP_SITE_NAME\"
-EDXAPP_CMS_SITE_NAME: \"$EDXAPP_CMS_SITE_NAME\"
-EDXAPP_PREVIEW_LMS_BASE: \"$EDXAPP_PREVIEW_LMS_BASE\"
-EOF"
+# bash -c "cat <<EOF >>server-vars.yml
+
+# EDXAPP_PLATFORM_NAME: \"$EDXAPP_PLATFORM_NAME\"
+# EDXAPP_SITE_NAME: \"$EDXAPP_SITE_NAME\"
+# EDXAPP_CMS_SITE_NAME: \"$EDXAPP_CMS_SITE_NAME\"
+# EDXAPP_PREVIEW_LMS_BASE: \"$EDXAPP_PREVIEW_LMS_BASE\"
+# EOF"
 
 
 
-bash -c "sudo sed -i '2iEDXAPP_PLATFORM_NAME_AZURE: \"$EDXAPP_PLATFORM_NAME\"' /edx/app/edx_ansible/server-vars.yml1"
-bash -c "sudo sed -i '2iEDXAPP_SITE_NAME_AZURE: \"$EDXAPP_SITE_NAME\"' /edx/app/edx_ansible/server-vars.yml1"
-bash -c "sudo sed -i '2iEDXAPP_CMS_SITE_NAME_AZURE: \"$EDXAPP_CMS_SITE_NAME\"' /edx/app/edx_ansible/server-vars.yml1"
-bash -c "sudo sed -i '2iEDXAPP_PREVIEW_LMS_BASE_AZURE: \"$EDXAPP_PREVIEW_LMS_BASE\"' /edx/app/edx_ansible/server-vars.yml1"
+bash -c "sed -i '2iEDXAPP_PLATFORM_NAME_AZURE: \"$EDXAPP_PLATFORM_NAME\"' /edx/app/edx_ansible/server-vars.yml"
+bash -c "sed -i '2iEDXAPP_SITE_NAME_AZURE: \"$EDXAPP_SITE_NAME\"' /edx/app/edx_ansible/server-vars.yml"
+bash -c "sed -i '2iEDXAPP_CMS_SITE_NAME_AZURE: \"$EDXAPP_CMS_SITE_NAME\"' /edx/app/edx_ansible/server-vars.yml"
+bash -c "sed -i '2iEDXAPP_PREVIEW_LMS_BASE_AZURE: \"$EDXAPP_PREVIEW_LMS_BASE\"' /edx/app/edx_ansible/server-vars.yml"
 
 wget https://raw.githubusercontent.com/edx/configuration/master/util/install/ansible-bootstrap.sh -O- | bash
 
