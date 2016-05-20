03100d96b000ef5b500eb0aa99af1083fea49784
Dmitry Gamanenko <dmitry.gamanenko@raccoongang.com>
2016-05-20 17:12:35 +0300

    update

--- openedx-fullstack-ubuntu/install-openedx.sh
+++ openedx-fullstack-ubuntu/install-openedx.sh
@@ -84,6 +84,20 @@ cd playbooks
 ansible-playbook -i localhost, -c local edx_sandbox.yml -e@$ANSIBLE_ROOT/server-vars.yml -e@$ANSIBLE_ROOT/extra-vars.yml
 
 
+# bash -c "cat <<EOF >edx-add-oauth2-backend.py
+# #!/usr/bin/env python
+# import os,sys
+# sys.path.append('/edx/app/edxapp/edx-platform')
+# os.environ['PATH'] = '/edx/app/edxapp/venvs/edxapp/bin:/edx/app/edxapp/edx-platform/bin:/edx/app/edxapp/.rbenv/bin:/edx/app/edxapp/.rbenv/shims:/edx/app/edxapp/.gem/bin:/edx/app/edxapp/edx-platform/node_modules/.bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+# os.environ['DJANGO_SETTINGS_MODULE'] = 'lms.envs.aws'
+# os.environ['SERVICE_VARIANT'] = 'lms'
+# from django.core.wsgi import get_wsgi_application
+# application = get_wsgi_application()
+# from third_party_auth.models import OAuth2ProviderConfig 
+# OAuth2ProviderConfig.objects.create(enabled=True, icon_class='fa-sign-in', name='microsoft', skip_registration_form=True, skip_email_verification=True, backend_name='microsoft-oauth2', key='$AZURE_AD_APP_KEY', secret='$AZURE_AD_APP_SECRET', other_settings='{}')
+# OAuth2ProviderConfig.objects.create(enabled=True, icon_class='fa-sign-in', name='cms-microsoft', skip_registration_form=True, skip_email_verification=True, backend_name='cms-microsoft-oauth2', key='$AZURE_AD_APP_KEY', secret='$AZURE_AD_APP_SECRET', other_settings='{}')
+# EOF"
+
 # sudo -Hu edxapp bash
 # source /edx/app/edxapp/edxapp_env 
 # python edx-add-oauth2-backend.py
