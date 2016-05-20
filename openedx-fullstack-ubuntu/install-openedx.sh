66192bd1746ae46cf4e120adb160c7d7c30001dc
Dmitry Gamanenko <dmitry.gamanenko@raccoongang.com>
2016-05-20 14:42:07 +0300

    add microsoft-oauth2 backend

--- openedx-fullstack-ubuntu/install-openedx.sh
+++ openedx-fullstack-ubuntu/install-openedx.sh
@@ -11,11 +11,17 @@ EDXAPP_SITE_NAME=$3
 EDXAPP_CMS_SITE_NAME=$4
 EDXAPP_PREVIEW_LMS_BASE=$5
 
+AZURE_AD_CLIENT_ID=$6
+AZURE_AD_APP_KEY=$7
+AZURE_AD_APP_SECRET=$8
+
+
 bash -c "sed -i '2iEDXAPP_PLATFORM_NAME_AZURE: \"$EDXAPP_PLATFORM_NAME\"' server-vars.yml"
 bash -c "sed -i '2iEDXAPP_SITE_NAME_AZURE: \"$EDXAPP_SITE_NAME\"' server-vars.yml"
 bash -c "sed -i '2iEDXAPP_CMS_SITE_NAME_AZURE: \"$EDXAPP_CMS_SITE_NAME\"' server-vars.yml"
 bash -c "sed -i '2iEDXAPP_PREVIEW_LMS_BASE_AZURE: \"$EDXAPP_PREVIEW_LMS_BASE\"' server-vars.yml"
 
+SECRET_KEY='XXX'
 # SECRET_KEY=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 bash -c "sed -i '2iEDXAPP_EDXAPP_SECRET_KEY_AZURE: \"$SECRET_KEY\"' server-vars.yml"
 
@@ -43,3 +49,18 @@ pip install -r requirements.txt
 
 cd playbooks
 ansible-playbook -i localhost, -c local edx_sandbox.yml -e@$ANSIBLE_ROOT/server-vars.yml -e@$ANSIBLE_ROOT/extra-vars.yml
+
+
+bash -c "cat <<EOF >edx-add-oauth2-backend.py
+#!/usr/bin/env python
+import os,sys
+sys.path.append('/edx/app/edxapp/edx-platform')
+os.environ['PATH'] = '/edx/app/edxapp/venvs/edxapp/bin:/edx/app/edxapp/edx-platform/bin:/edx/app/edxapp/.rbenv/bin:/edx/app/edxapp/.rbenv/shims:/edx/app/edxapp/.gem/bin:/edx/app/edxapp/edx-platform/node_modules/.bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'lms.envs.aws'
+os.environ['SERVICE_VARIANT'] = 'lms'
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+from third_party_auth.models import OAuth2ProviderConfig 
+OAuth2ProviderConfig.objects.create(enabled=True, icon_class='fa-sign-in', name='microsoft', skip_registration_form=True, skip_email_verification=True, backend_name='microsoft-oauth2', key='$AZURE_AD_APP_KEY', secret='$AZURE_AD_APP_SECRET', other_settings='{}')
+OAuth2ProviderConfig.objects.create(enabled=True, icon_class='fa-sign-in', name='cms-microsoft', skip_registration_form=True, skip_email_verification=True, backend_name='cms-microsoft-oauth2', key='$AZURE_AD_APP_KEY', secret='$AZURE_AD_APP_SECRET', other_settings='{}')
+EOF"
